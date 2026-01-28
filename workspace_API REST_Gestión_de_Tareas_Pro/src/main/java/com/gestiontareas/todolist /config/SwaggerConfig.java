07f81ebd6d7be57e774441cf45e8b2d4580dae93
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-01-28 14:29:53 +0100

    Delete workspace_API REST_Gestión_de_Tareas_Pro/src/main/java/com/gestiontareas directory

--- workspace_API REST_Gestión_de_Tareas_Pro/src/main/java/com/gestiontareas/todolist /config/SwaggerConfig.java
+++
@@ -1,8 +0,0 @@
-package com.gestiontareas.todolist.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration	// Indica que esta clase es una clase de configuración de Spring
-public class SwaggerConfig {
-
-}
