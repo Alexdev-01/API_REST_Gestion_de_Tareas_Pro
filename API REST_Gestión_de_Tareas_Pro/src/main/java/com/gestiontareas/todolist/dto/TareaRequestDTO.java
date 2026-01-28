b3ab31723a2188b645edeaba71a0cc4bc0085596
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-01-28 14:37:07 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Pro/src/main/java/com/gestiontareas/todolist/dto/TareaRequestDTO.java
+++ API REST_Gestión_de_Tareas_Pro/src/main/java/com/gestiontareas/todolist/dto/TareaRequestDTO.java
@@ -1,1 +1,13 @@
+package com.gestiontareas.todolist.dto;
 
+import lombok.Data;
+
+//Representa lo que entra por POST o PUT
+@Data // Clase DTO para las solicitudes de creación o actualización de tareas
+public class TareaRequestDTO {
+	
+	private String Titulo;
+	private String descripcion;
+	private Long usuarioId;
+
+}
---
+++ API REST_Gestión_de_Tareas_Pro/src/main/java/com/gestiontareas/todolist/dto/TareaResponseDTO.java
@@ -0,0 +1,18 @@
+package com.gestiontareas.todolist.dto;
+
+import java.time.LocalDateTime;
+import com.gestiontareas.todolist.model.EstadoTarea;
+import lombok.Data;
+
+//Representa lo que sale por GET al cliente
+@Data // Clase DTO para las respuestas de las tareas
+public class TareaResponseDTO {
+	
+	private Long id;
+	private String Titulo;
+	private String descripcion;
+	private EstadoTarea estado;
+	private LocalDateTime fechaCreacion;
+	private Long usuarioId;
+
+}
